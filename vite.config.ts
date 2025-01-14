e8f1caad6b9c970aac1ce7af5bc94f59804b19c0
Андрей Крамарчук <inf1nitiveit@gmail.com>
2025-01-15 00:26:19 +0300

    Вносит настройки для деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/RC-24-second-project/',
+  build: {
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
